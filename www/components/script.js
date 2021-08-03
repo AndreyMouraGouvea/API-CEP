70108ba934f4027263954ba6837687eb5529e55a
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-08-03 23:50:42 +0900

    update

--- www/components/script.js
+++ www/components/script.js
@@ -1,2 +1,20 @@
-// This is a JavaScript file
+$(document).on("click","#buscar",function(){
+  $.ajax({
+    type:"get",//como vou enviar
+    url: "http://viacep.com.br/ws/"+$("#cep").val()+"/json/", //para onde enviar
+    success: function(data){  //executar comando caso funcione
+      var conteudo;
+      conteudo = "<p><strong>Logradouro:</strong>"+data.logradouro+"</p>";
+      conteudo += "<p><strong>Bairro:</strong>"+data.bairro+"</p>";
+      conteudo += "<p><strong>Localidade:</strong>"+data.localidade+"</p>";
+      conteudo += "<p><strong>Estado:</strong>"+data.uf+"</p>";
+
+      $("dados").html(conteudo);
+    },
+    error: function(){
+      alert("Error!"); //executar comando caso não funcione
+    }
+    })
+  
+});
 
